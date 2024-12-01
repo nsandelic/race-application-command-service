990092166e4671a5502b8d1e7e71aa4b11e626c5
Nikola Sandelic <sandelic.nikola@gmail.com>
2024-12-01 17:44:19 +0100

    init

---
+++ src/main/java/hr/spring/race/application/command/service/RaceApplicationCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package hr.spring.race.application.command.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceApplicationCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceApplicationCommandServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/spring/race/application/command/service/RaceApplicationCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.spring.race.application.command.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceApplicationCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
