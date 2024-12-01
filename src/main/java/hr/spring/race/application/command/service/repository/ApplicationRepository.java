68107580cf162e0b261607c6feeaba5b39140016
Nikola Sandelic <sandelic.nikola@gmail.com>
2024-12-01 20:31:42 +0100

    Small change.

--- src/main/java/hr/spring/race/application/command/service/repository/ApplicationRepository.java
+++ src/main/java/hr/spring/race/application/command/service/repository/ApplicationRepository.java
@@ -2,9 +2,11 @@ package hr.spring.race.application.command.service.repository;
 
 import hr.spring.race.application.command.service.model.entity.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ApplicationRepository extends JpaRepository<Application, UUID> {
 
 }
